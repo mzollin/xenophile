b5aae321d2f3423c871f7e10688a14e877c71a34
Marco Zollinger <marco@freelabs.space>
2018-06-14 15:43:53 +0200

    added rotation script

---
+++ Assets/animation_rotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animation_rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        Transform transform = GetComponent<Transform>();
+        Vector3 k_ModelRotation = new Vector3(0.0f, 1.0f, 0.0f);
+        transform.Rotate(k_ModelRotation * Time.deltaTime);
+    }
+}
