3afeaa4378f41dac60c11523caabb9a362ca3abb
SudoRB <40261119+SudoRB@users.noreply.github.com>
2018-06-16 15:16:18 +0200

    Delete Button Added to Obj

---
+++ Assets/DeleteAction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeleteAction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TaskOnClick()
+    {
+        Destroy(GetComponent<Transform>().parent);
+    }
+}
