fe6dc98a32ec49c0520045aceaa9353e49a348ee
SudoRB <40261119+SudoRB@users.noreply.github.com>
2018-06-18 15:13:42 +0200

    Added pod and proximity sensor

---
+++ Assets/ProximitySensor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProximitySensor : MonoBehaviour {
+
+    public GameObject obj:
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 campos = obj.GetComponent<Transform>().position;
+        Vector3 pos = GetComponent<Transform>().position;
+        if(Vector3.Distance(campos, pos) <= 1.5)
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+        }
+        else
+        {
+
+            GetComponent<MeshRenderer>().enabled = true;
+        }
+	}
+}
