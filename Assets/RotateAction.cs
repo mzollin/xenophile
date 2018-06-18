4eb391427926f01719c049854a2d2dc71a1c77f4
SudoRB <40261119+SudoRB@users.noreply.github.com>
2018-06-18 21:55:01 +0200

    rotation start/stop

---
+++ Assets/RotateAction.cs
@@ -0,0 +1,48 @@
+﻿using GoogleARCore;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAction : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // If the player has not touched the screen, we are done with this update.
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        // Raycast against the location the player touched to search for planes.
+        TrackableHit hit;
+        TrackableHitFlags raycastFilter = TrackableHitFlags.None;
+
+        if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+        {
+            if ((hit.Trackable.Equals(this)))
+            {
+                TaskOnClick();
+            }
+        }
+
+    }
+
+    void TaskOnClick()
+    {
+        animation_rotate rot = GetComponent<animation_rotate>();
+        if (rot != null)
+        {
+            rot.rotate = !rot.rotate;
+        }
+    }
+
+
+}
